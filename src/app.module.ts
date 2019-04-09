456d18825f7ee5ec75670f12aa3576ffdb02c9eb
chahhajar <chah.hajar@gmail.com>
2019-04-09 18:06:25 +0100

    qsdqsdq

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import 'reflect-metadata';
+import { join } from 'path';
+import { EventsModule } from './events/events.module';
+
+@Module({
+  imports: [
+    EventsModule,
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/graphql.ts
@@ -0,0 +1,10 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+export interface IQuery {
+    temp__(): boolean | Promise<boolean>;
+}
